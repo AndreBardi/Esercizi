c28da37a5aaf2998256daa70f66d211bcb1d1152
AndreBardi <andrea_bardi@yahoo.com>
2024-04-24 20:00:02 +0200

    prova

---
+++ esercizi/lezione_3/prova.py
@@ -0,0 +1,8 @@
+def third_max(nums: list[int]) -> int:
+    # elimina il pass e inserisci il tuo codice
+    # potete utilizzare queste tre variabili di comodo
+    first_max = second_max = third_max = float('-inf')
+    third_max = [1, 2, 3, 4]
+
+    my_set = set(third_max)
+    print(my_set)
