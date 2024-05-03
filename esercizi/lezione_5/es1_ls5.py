9dc09ac460dfb4e3d7613f5ca42370658f862084
AndreBardi <andrea_bardi@yahoo.com>
2024-05-03 14:01:36 +0200

    es1_ls5 (modify)

--- esercizi/lezione_5/es1_ls5.py
+++ esercizi/lezione_5/es1_ls5.py
@@ -1,14 +1,18 @@
 def visiting_tree_iterative(tree: dict[int, list[int]], root: int):
     result = {}
-    stack: list[int] = [root]
+    node_number = {}
+    curr_level = {}
+    node_number_per_level = {}
+    stack: list[int] = [(root, 0)]
     while stack: # while len(stack) != 0
-        curr_node = stack.pop(0)
+     """   curr_node = stack.pop(0)
+        result[curr_level] = result.get(curr_level, 0) + curr_level
+        node_number_per_level[curr_level] = node_number_per_level.get(curr_level, 0) + 1
         if curr_node:
             print(curr_node)
-            left_child, right_child =\
-                tree[curr_node]
+            left_child, right_child = tree[curr_node]
             if left_child:
-                stack.append(left_child)
+                stack.append(left_child,)
             if right_child:
                 stack.append(right_child)
-    return result
+    return result """
