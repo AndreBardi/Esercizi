eb7bffafa43ced2f2fea5507bfde1655c67b8807
AndreBardi <andrea_bardi@yahoo.com>
2024-05-08 19:21:27 +0200

    modifing zoo.zip

--- AndreaBardi.zip/zoo.py
+++ AndreaBardi.zip/zoo.py
@@ -4,8 +4,23 @@ class Zoo:
         self.zoo: str = zoo
         self.zoo_keepers: str = zoo_keepers
 
-    def animal(self,
-            name: str, species: str, age: str, height: str, 
-            width: str, preferred_habitat: str, health: str):
+class Animal:
+    def __init__(self,
+            name: str, species: str, age: int, height: float, 
+            width: float, preferred_habitat: str, health: float):
+       
         self.name: str = name
         self.species: str = species
+        self.age: int = age
+        self.height: float = height
+        self.width: float = width
+        self.preferred_habitat: str = preferred_habitat
+        self.health: float = health
+
+class Fence:
+    def __init__(self, area: float, temperature: 
+                 float, habitat: str) -> list:
+        self.area: float = area
+        self.temperature: float = temperature
+        self.habitat: str = habitat
+    
