b868e75e0e85763b583c6ceb7e4a8fc74358b575
AndreBardi <andrea_bardi@yahoo.com>
2024-05-28 13:54:55 +0200

    changes tartaruga_e_lepre

--- esercizi/lezione_7/tartaruga_e_lepre.py
+++ esercizi/lezione_7/tartaruga_e_lepre.py
@@ -1,14 +1,17 @@
 import random
 
-def position(t_position: int,
-             h_position: int,
-             path: list[int]):
-    
-    path: list[int] = ["_"] * 70
-    path[t_position[0]] = "T"
-    path[h_position[0]] = "H"
+def position(t_position: int, h_position: int) -> list[str]:
+    path = ["_"] * 70
+    if t_position < len(path):
+        path[t_position] = "T"
+    if h_position < len(path):
+        path[h_position] = "H"
+    if t_position == h_position:
+        path[t_position] = "OUCH!!!"
+        path[h_position] = "OUCH!!!"
+    return path
 
-def t_move(t_position):
+def t_move(t_position: int) -> int:
     i = random.randint(1, 10)
     if 1 <= i <= 5:
         t_position += 3
@@ -16,9 +19,13 @@ def t_move(t_position):
         t_position -= 6
     if 8 <= i <= 10:
         t_position += 1
-    return t_position        
+    if i < 1:
+        t_position = 1
+    if i > 70:
+        t_position = 70
+    return t_position
 
-def h_move(h_position):
+def h_move(h_position: int) -> int:
     i = random.randint(1, 10)
     if 1 <= i <= 2:
         h_position += 0
@@ -30,6 +37,11 @@ def h_move(h_position):
         h_position += 1
     if 9 <= i <= 10:
         h_position -= 2
+    if i < 1:
+        h_position = 1
+    if i > 70:
+        h_position = 70
+    return h_position
 
 def begin_race():
     t_position = 1
@@ -38,4 +50,20 @@ def begin_race():
     print("BANG !!!!! AND THEY'RE OFF !!!!!")
 
     while True:
-        pass
+        t_position = t_move(t_position)
+        h_position = h_move(h_position)
+        path = position(t_position, h_position)
+        
+        print("".join(path))
+
+        if t_position >= 70:
+            print("TORTOISE WINS | YAY!!!")
+            break
+        if h_position >= 70:
+            print("HARE WINS | YUCH!!!")
+            break
+        if t_position >= 70 and h_position >= 70:
+            print("IT'S A TIE!!!")
+            break
+
+begin_race()
