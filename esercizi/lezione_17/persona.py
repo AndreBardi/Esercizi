345a654c14268cd2457e12d6567a11497ee00133
AndreBardi <andrea_bardi@yahoo.com>
2024-06-14 11:38:54 +0200

    persona.py

---
+++ esercizi/lezione_17/persona.py
@@ -0,0 +1,12 @@
+class Persona:
+    def __init__(self, first_name: str, last_name: str) -> None:
+        if first_name is type == str:
+            age = 0
+        else:
+            age = None
+            return "Il nome inserito non è una stringa!"
+        if last_name is type == str:
+            age = 0
+        else: 
+            age = None
+            return "Il nome inserito non è una stringa!"
