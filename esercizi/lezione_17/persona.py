f4e7662c4b34416c628785298764ca7dfaa92fa7
AndreBardi <andrea_bardi@yahoo.com>
2024-06-15 12:45:00 +0200

    persona.py finito

--- esercizi/lezione_17/persona.py
+++ esercizi/lezione_17/persona.py
@@ -1,12 +1,48 @@
 class Persona:
     def __init__(self, first_name: str, last_name: str) -> None:
-        if first_name is type == str:
-            age = 0
+        if type(first_name) is str:
+            self.first_name = first_name
         else:
-            age = None
-            return "Il nome inserito non è una stringa!"
-        if last_name is type == str:
-            age = 0
-        else: 
-            age = None
-            return "Il nome inserito non è una stringa!"
+            self.first_name = None
+            print("Il nome inserito non è una stringa!")
+
+        if type(last_name) is str:
+            self.last_name = last_name
+        else:
+            self.last_name = None
+            print("Il nome inserito non è una stringa!")
+
+        if first_name is not None and last_name is not None:
+            self.età = 0
+        else:
+            self.età = None
+    
+    def setName(self, first_name):
+       if type(first_name) is str:
+           self.first_name = first_name
+       else:
+           print("Il nome inserito non è una stringa!")
+    
+    def setLastName(self, last_name):
+        if type(last_name) is str:
+            self.last_name = last_name
+        else:
+            print("Il cognome inserito non è una stringa!")
+
+    def setAge(self, età):
+        if type(età) is int:
+            self.età = età
+        else:
+            print("L'età deve essere un numero intero!")
+
+    def getName(self):
+        return self.first_name
+    
+    def getLastname(self):
+        return self.last_name
+    
+    def getAge(self):
+        return self.età
+    
+    def greet(self):
+        print(f'Ciao, sono {self.first_name} {self.last_name}! Ho {self.età} anni!')
