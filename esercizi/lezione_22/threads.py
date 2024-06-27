d00cef5f3a971f75cb2d4022a8ab37adc587c870
AndreBardi <andrea_bardi@yahoo.com>
2024-06-27 20:03:22 +0200

    Threads

---
+++ esercizi/lezione_22/threads.py
@@ -0,0 +1,33 @@
+import concurrent.futures
+import threading
+import time
+import concurrent
+
+def thread_function(name):
+    print(f'Thread {name} starting at {time.time}')
+    time.sleep(2)
+    print(f'Thread {name} ending at {time.time}')
+""" 
+if __name__ == "__main__":
+
+    x = threading.Thread(target=thread_function, args=(1,))
+    print("Main    : before running thread")
+    x.start()
+    print("Main    : after running thread")
+    x.join()
+    print("Main    : all done") """
+    
+""" threads = list()
+for index in range(3):
+    print(f'Main    : before running thread')
+    x = threading.Thread(target=thread_function, args=(index,))
+    threads.append(x)
+    x.start()
+
+for index, thread in enumerate(threads):
+    print('Main    : waiting for the thread to finish')
+    thread.join()
+    print('Main    : all done') """
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+    executor.map(thread_function, range(3))
