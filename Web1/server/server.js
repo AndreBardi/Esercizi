21d777ed23549adf68e13125a2f7989959a5744b
AndreBardi <andrea_bardi@yahoo.com>
2024-07-25 13:57:54 +0200

    form web1

--- Web1/server/server.js
+++ Web1/server/server.js
@@ -17,20 +17,39 @@ res.sendFile("formSemplice.html", { root: './htdoc' });
 });
 
 app.get('/gestisciDatiForm', (req, res) => {
-    console.log(req.query.fname);
-    res.send("<html>Buona serata " + req.query.fname + " " + req.query.fcognome + "</html>");
-    });
-
-app.get('/sendFile', (req, res) => {
-    console.log("Mi hai chiesto la form ");
-    res.sendFile("sendFile.html", {root: '/htdoc'});
-    });
-
-app.post('/mansendfile', (req, res) => {
-    console.log(req.body.password)
-    pass_ricevuta = req.body.password;
-    if (pass_ricevuta == 'paperino')
-        res.send("<html> Bravo "+ req.body.email + "<br> Sono pronto a ricevere il file!</html>")
+    console.log(req.query.fname, req.query.fcognome);
+    response = "<html>Buona serata " + req.query.fname + " " + req.query.fcognome;
+    response += "<br>La tua città è " + req.query.fComune
+    if(req.query.fsesso == "1")
+        response += "<br>Sei una femmina"
     else
-        res.send("<html> Buona sera" + req.body.fname + "</html>")
-});
+        response += "<br>Sei un maschio"
+    response += "<br> ti voglio bene"+"</html>";
+    res.send(response);
+    });
+
+app.post('/gestisciDatiForm', (req, res) => {
+    console.log(req.body.fname, req.body.fcognome);
+    response = "<html>Buona serata " + req.body.fname + " " + req.body.fcognome;
+    response += "<br>La tua città è " + req.body.fComune
+    if(req.body.fsesso == "1")
+        response += "<br>Sei una femmina"
+    else
+        response += "<br>Sei un maschio"
+    response += "<br> ti voglio bene"+"</html>";
+    res.send(response);
+    });
+
+// app.get('/sendfile', (req, res) => {
+//     console.log("Mi hai chiesto la form ");
+//     res.sendFile("sendfile.html", {root: './htdoc'});
+//     });
+
+// app.post('/mansendfile', (req, res) => {
+//     console.log(req.body.password)
+//     pass_ricevuta = req.body.password;
+//     if (pass_ricevuta == 'paperino')
+//         res.send("<html> Bravo "+ req.body.email + "<br> Sono pronto a ricevere il file!</html>")
+//     else
+//         res.send("<html> Buona sera" + req.body.fname + "</html>")
+// });
