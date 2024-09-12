3e2d1504c72b8c4bf79b6418a4c45f6e2f98fe08
AndreaBardi <andrea_bardi@yahoo.com>
2024-09-12 19:31:02 +0200

    Lezione 12/09

--- Accademia4.sql
+++ Accademia4.sql
@@ -1,59 +1,61 @@
 ---QUERY 1
 
 SELECT cognome
-FROM Persona
+FROM Persona;
 
 ---QUERY 2
 
 SELECT nome, cognome
 FROM Persona
-WHERE posizione = 'Ricercatore'
+WHERE posizione = 'Ricercatore';
 
 ---QUERY 3
 
 SELECT cognome
 FROM Persona
 WHERE posizione = 'Professore Associato'
-AND cognome LIKE 'V%'
+AND cognome LIKE 'V%';
 
 ---QUERY 4
 
 SELECT cognome
 FROM Persona
 WHERE (posizione = 'Professione Associato' OR posizione = 'Professore Ordinario')
-AND cognome LIKE 'V%'
+AND cognome LIKE 'V%';
     
 ---QUERY 5
 
 SELECT nome
 FROM Progetto
-WHERE fine < CURRENT_DATE
+WHERE fine < CURRENT_DATE;
 
 ---QUERY 6
 
 SELECT nome
 FROM Progetto
-ORDER BY inizio ASC
+--WHERE TRUE (opzionale)
+ORDER BY inizio ASC;
 
 ---QUERY 7
 
 SELECT nome
 FROM WP
-ORDER BY nome ASC
+--WHERE TRUE (opzionale)
+ORDER BY nome ASC;
 
 ---QUERY 8
 
 SELECT DISTINCT tipo
-FROM Assenza
+FROM Assenza;
 
 ---QUERY 9
 
 SELECT DISTINCT tipo
-FROM AttivitàProgetto
+FROM AttivitàProgetto;
 
 --QUERY 10
 
 SELECT DISTINCT giorno
 FROM AttivitaNonProgettuale
 WHERE tipo = 'Didattica'
-ORDER BY giorno ASC
+ORDER BY giorno ASC;
