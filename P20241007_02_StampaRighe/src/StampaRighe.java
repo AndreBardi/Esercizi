f09f6003c2f60be3152b99d139b73b0d564dbb1d
AndreaBardi <andrea_bardi@yahoo.com>
2024-10-07 13:13:36 +0200

    Cancellazione di cartella Java e ripristino precedente format di salvataggio

---
+++ P20241007_02_StampaRighe/src/StampaRighe.java
@@ -0,0 +1,25 @@
+
+public class StampaRighe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Prima Riga");
+		System.out.println("Seconda Riga");
+		System.out.println("Terza Riga");
+		
+		byte b1 = 10;
+		byte b2;
+		b2 = 100;
+		byte b3, b4, b5;
+		
+		System.out.println("Il valore di b1 è di: " + b1);
+		b1 = 127;
+		System.out.println("Il valore di b1 è di: " + b1);
+		b1 += 1;
+		System.out.println("Il valore di b1 è di: " + b1);
+		
+		
+	}
+
+}
