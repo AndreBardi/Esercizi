160318fb7d361458d662b20c9d19c3989c877229
AndreBardi <andrea_bardi@yahoo.com>
2024-10-26 21:20:20 +0200

    Accademia8 (da finire)

---
+++ Accademia8.sql
@@ -0,0 +1,49 @@
+--query 1
+
+SELECT distinct p.id , p.nome , p.cognome
+FROM persona AS p
+JOIN Assenza  AS ass
+on ass.persona = p.id
+full  OUTER JOIN Attivitaonprogettuale AS a1 
+on a1.giorno = ass.giorno 
+full OUTER JOIN AttivitaProgetto AS a2
+on a2.giorno = ass.giorno 
+GROUP BY p.id
+HAVING count(a1.giorno) = 0
+AND count(a2.giorno)= 0;
+
+--query 2
+
+SELECT p.id ,p.nome, p.cognome
+FROM persona AS p
+LEFT OUTER JOIN AttivitaProgetto AS ap ON p.id = ap.persona
+LEFT OUTER JOIN progetto AS pr ON pr.id = ap.progetto 
+AND pr.nome = 'Pegasus'
+GROUP BY p.id
+HAVING count(ap.progetto) = 0;
+
+--query3
+
+  WITH tot_sti AS (
+    SELECT max(stipendio) AS max_sti
+    FROM persona
+    WHERE  (posizione ='Professore Associato' or posizione = 'Professore Ordinario')
+)
+SELECT p.id , p.nome , p.cognome , p.stipendio
+FROM persona AS p , tot_sti AS ts
+WHERE p.posizione = 'Ricercatore'
+AND ts.max_sti < p.stipendio
+GROUP BY p.id
+
+--query4
+WITH bdg_tot AS (
+    SELECT avg(pr.budget) AS max_bdg
+    FROM progetto AS pr 
+)
+	
+SELECT p.id,p.nome,p.cognome
+FROM persona AS p, progetto AS pr, AttivitaProgetto AS ap, bdg_tot AS bt
+WHERE pr.id = ap.progetto
+AND p.id = ap.persona
+AND bt.max_bdg < pr.budget
+GROUP BY p.id;
